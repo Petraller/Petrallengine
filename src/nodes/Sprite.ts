dc0a0c83c615f14d4d1edc948045aaa3eca1c5e9
Clarence Tay <me@petraller.com>
2023-10-13 10:35:35 +0800

    Better Sprite blending

--- src/nodes/Sprite.ts
+++ src/nodes/Sprite.ts
@@ -16,8 +16,7 @@ export default class Sprite extends Node implements IDrawable {
     private _image: string | null = null;
     private _color: Color = Color.white;
     private bitmap: ImageBitmap | null = null;
-    private workingCanvas = new OffscreenCanvas(256, 256);
-    private workingContext: OffscreenCanvasRenderingContext2D | null = null;
+    private workingCanvas: [OffscreenCanvas, OffscreenCanvas] = [new OffscreenCanvas(1, 1), new OffscreenCanvas(1, 1)];
 
     /** The normalized pivot. */
     pivot: Vec2 = Vec2.multiply(Vec2.one, 0.5);
@@ -26,17 +25,22 @@ export default class Sprite extends Node implements IDrawable {
         if (this.bitmap) {
             context.save();
             context.translate(-this.pivot.x * this.bitmap.width, -this.pivot.y * this.bitmap.height);
-            if (this.workingContext) {
-                this.workingContext.globalCompositeOperation = "copy";
-                this.workingContext.drawImage(this.bitmap, 0, 0);
-                this.workingContext.globalCompositeOperation = "multiply";
-                this.workingContext.fillStyle = this.color.toHexString();
-                this.workingContext.fillRect(0, 0, this.workingCanvas.width, this.workingCanvas.height);
-                this.workingContext.globalCompositeOperation = "destination-atop";
-                this.workingContext.drawImage(this.bitmap, 0, 0);
-            }
+
+            const workingContext = this.workingCanvas.map(e => e.getContext('2d')!);
+            workingContext[0].globalCompositeOperation = "copy";
+            workingContext[0].fillStyle = this.color.toHexString();
+            workingContext[0].fillRect(0, 0, this.workingCanvas[0].width, this.workingCanvas[0].height);
+            workingContext[0].globalCompositeOperation = "destination-in";
+            workingContext[0].drawImage(this.bitmap, 0, 0);
+            workingContext[1].globalCompositeOperation = "copy";
+            workingContext[1].drawImage(this.bitmap, 0, 0);
+            workingContext[1].globalCompositeOperation = "multiply";
+            workingContext[1].drawImage(this.workingCanvas[0], 0, 0);
+            workingContext[1].globalCompositeOperation = "destination-atop";
+            workingContext[1].drawImage(this.bitmap, 0, 0);
+
             context.globalAlpha = this.color.a;
-            context.drawImage(this.workingCanvas, 0, 0);
+            context.drawImage(this.workingCanvas[1], 0, 0);
             context.restore();
         }
     }
@@ -61,8 +65,10 @@ export default class Sprite extends Node implements IDrawable {
             return;
         }
         const bmp = await Sprite.load(this._image);
-        this.workingCanvas = new OffscreenCanvas(bmp.width, bmp.height);
-        this.workingContext = this.workingCanvas.getContext('2d');
+        for (const cnv of this.workingCanvas) {
+            cnv.width = bmp.width;
+            cnv.height = bmp.height;
+        }
         this.bitmap = bmp;
     }
 
